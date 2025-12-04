1dd2248417d27037f648ae0eed8aad01d5c46f44
Aaliyah Harper <aaliyah@aileks.dev>
2025-12-04 14:18:32 -0500

    fix system prompt

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from prompts import system_prompt
+from prompts import SYSTEM_PROMPT
 
 
 load_dotenv()
@@ -22,7 +22,7 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model="gemini-2.5-flash",
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT),
 )
 
 prompt_tokens: int | None = (
